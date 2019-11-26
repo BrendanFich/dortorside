0ce29c3b56fcc4b13b17adfefed7538efad23deb
陈俊坚 <junjianch@gmail.com>
2019-11-26 17:51:32 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-view/>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="sass" rel="stylesheet/sass">
+@import '~assets/sass/variable'
+html, body
+  height: 100%
+  margin: 0
+  padding: 0
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif
+</style>
---
+++ src/components/Index/Index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="index">
+    <div class="top"></div>
+    <div class="main">
+      <div class="left"></div>
+      <div class="mian">
+        <div class="menu"></div>
+        <div class="menuBtn"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import '~assets/sass/mixin'
+@import '~assets/sass/variable'
+.index
+  width: 100%
+  height: 100vh
+  background: $color-white
+</style>
